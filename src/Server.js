155e5750bc50fbf802e3343f210e140cf3d76906
johnnjoroge <jn@43kare.de>
2017-12-08 15:36:11 +0200

    g

--- src/Server.js
+++ src/Server.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 3001);
+http.listen(process.env.PORT || 3000, function(){
+    console.log('listening on', http.address().port);
+  });
 
 
 
