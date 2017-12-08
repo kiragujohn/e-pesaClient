7d9ba74559c29bacced7a4b52bd38bcb7f972750
johnnjoroge <jn@43kare.de>
2017-12-08 11:08:54 +0200

    dfd

--- server.js
+++ server.js
@@ -13,5 +13,6 @@ const path = require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  //res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile('public/index1.html' , { root : __dirname});
 });
