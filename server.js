f40c2d9b9b11217cd96bfcf76beda8e0df21811b
johnnjoroge <jn@43kare.de>
2017-12-09 10:37:28 +0200

    f

--- server.js
+++ server.js
@@ -9,7 +9,5 @@ app.listen(process.env.PORT || 5000);
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname +'/dist/index.html'));
 })
-
-conslole.log('Console Listening!');
-
-
+console.log('listening on', http.address().port);
+ 
