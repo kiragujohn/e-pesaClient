53e2f9f6d040f8e383d45ddfa32369beaba1477b
johnnjoroge <jn@43kare.de>
2017-12-09 12:14:11 +0200

    s

--- Server.js
+++ Server.js
@@ -5,8 +5,7 @@ var express = require('express');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
-
-  var server = app.listen(process.env.PORT || 5000, function () {
-    var port = server.address().port;
-    console.log("Express is working on port " + port);
+ 
+  app.listen(process.env.PORT || 3000, function(){
+    //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
