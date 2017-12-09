183dcc7e4ceed238965d674d91de2687252d673d
johnnjoroge <jn@43kare.de>
2017-12-09 12:41:58 +0200

    f

--- Server.js
+++ Server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+var http = require('http');
 var express = require('express');
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
- 
-  app.listen(process.env.PORT || 3000, function(){
-    //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+
+
+
+http.listen((process.env.PORT || 5000), function(){
+  console.log('listening on *:5000');
+});
+
