8910ea9cf720c7da0b8bbd36bcadbf97c50042db
johnnjoroge <jn@43kare.de>
2017-12-09 11:25:26 +0200

    Changed port number

--- Server.js
+++ Server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + './dist'));
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-http.listen(process.env.PORT || 5000, function(){
+http.listen(process.env.port || 5000, function(){
     console.log('listening on', http.address().port);
   });
