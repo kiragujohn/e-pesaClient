e0368aadb690832b722d302182f47c5a5cd03ebb
johnnjoroge <jn@43kare.de>
2017-12-09 12:04:00 +0200

    g

--- Server.js
+++ Server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-var http = require('http');
 var express = require('express');
 
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-http.listen(process.env.port || 5000, function(){
-    console.log('listening on', http.address().port);
+
+  var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
   });
